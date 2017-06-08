23d98e900299129f17fc16c9043fcb42785492e7
Arpit Shah <arpit@Trinas-MacBook-Pro.local>
2017-06-08 02:09:44 -0700

    connect mongoose and mongoDB

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = require('./router')
+
+//DB Setup
+mongoose.connect('mongodb://localhost:auth/auth');
 
 //App Setup
 app.use(morgan('combined')); // Logging
